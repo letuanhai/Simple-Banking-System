c0dd7718f2ffda6ebe57a0ad39ebd894e6882191
letuanhai <letuanhai@live.com>
2021-01-16 22:04:22 +0700

    Implemented Luhn algorithm

--- Simple Banking System/task/banking/banking.py
+++ Simple Banking System/task/banking/banking.py
@@ -1,4 +1,4 @@
-# Write your code here
+import random
 
 WELCOME_PROMPT = """
 1. Create an account
@@ -11,36 +11,42 @@ LOGGED_IN_PROMPT = """
 0. Exit
 """
 
+
 class Account:
     accounts = {}
+
     # Dictionary of card_number:Account object pairs
     def __init__(self):
-        card_number = "400000" + format(random.randint(0, 9999999), "09")
-        card_digits = []
-
-def create_account(acc_list):
-    card_number = "400000" + format(random.randint(0, 9999999), "09") + "0"
-    while card_number in acc_list:
-        card_number = (
-            "400000"
-            + format(random.randint(0, 9999999), "09")
-            + str(random.randint(1, 9))
-        )
-    acc_list[card_number] = (format(random.randint(0, 9999), "04"), 0)
-    print(f"Your card has been created\nYour card number:\n{card_number}")
-    print(f"Your card PIN:\n{acc_list[card_number][0]}")
-    return acc_list
+        card_number = "400000" + format(len(Account.accounts), "09")
+        digits = []
+        for i, d in enumerate(card_number):
+            if i % 2 == 0:
+                digits.append(2 * int(d) if 2 * int(d) <= 9 else 2 * int(d) - 9)
+            else:
+                digits.append(int(d) if int(d) <= 9 else int(d) - 9)
+        checksum = 0 if sum(digits) % 10 == 0 else 10 - (sum(digits) % 10)
+        card_number = card_number + str(checksum)
+        self.card_number = card_number
+        self.pin = format(random.randint(0, 9999), "04")
+        self.balance = 0
+        Account.accounts[card_number] = self
 
 
-def log_in(acc_list):
+def create_account():
+    new_account = Account()
+    print(f"Your card has been created\nYour card number:\n{new_account.card_number}")
+    print(f"Your card PIN:\n{new_account.pin}")
+
+
+def log_in():
     card_number = input("Enter your card number:")
     pin_code = input("Enter your PIN:")
-    if acc_list.get(card_number, ["a"])[0] == pin_code:
-        print("You have successfully logged in!")
-        return True, card_number
-    else:
-        print("Wrong card number or PIN!")
-        return False, card_number
+    if card_number in Account.accounts:
+        if Account.accounts[card_number].pin == pin_code:
+            print("You have successfully logged in!")
+            return True, Account.accounts[card_number]
+    print("Wrong card number or PIN!")
+    return False, card_number
 
 
 logged_in = False
@@ -50,12 +56,12 @@ while choice != 0:
     choice = int(input(LOGGED_IN_PROMPT if logged_in else WELCOME_PROMPT))
     if not logged_in:
         if choice == 1:
-            create_account(accounts)
+            create_account()
         elif choice == 2:
-            logged_in, account_id = log_in(accounts)
+            logged_in, account_id = log_in()
     else:
         if choice == 1:
-            print("Balance: ", accounts[account_id][1])
+            print("Balance: ", account_id.balance)
         else:
             logged_in = False
 
