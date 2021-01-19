561ef9abcdf652114e0129d611b2a4ec81739a42
letuanhai <letuanhai@live.com>
2021-01-19 23:19:22 +0700

    Stage 4: adding new features

--- Simple Banking System/task/banking/banking.py
+++ Simple Banking System/task/banking/banking.py
@@ -4,9 +4,8 @@ import sqlite3
 conn = sqlite3.connect("card.s3db")
 cur = conn.cursor()
 
-cur.execute("DROP TABLE card;")
 cur.execute(
-    "CREATE TABLE card (id INTEGER, number TEXT, pin TEXT, balance INTEGER DEFAULT 0);"
+    "CREATE TABLE IF NOT EXISTS card (id INTEGER, number TEXT, pin TEXT, balance INTEGER DEFAULT 0);"
 )
 conn.commit()
 
@@ -17,12 +16,52 @@ WELCOME_PROMPT = """
 """
 LOGGED_IN_PROMPT = """
 1. Balance
-2. Log out
+2. Add income
+3. Do transfer
+4. Close account
+5. Log out
 0. Exit
 """
 
+class Account:
+    def __init__(self, number, pin, balance):
+        self.card_number = number
+        self.pin = pin
+        self.balance = balance
 
-def new_card(number_of_account):
+    def check_balance(self):
+        print('Balance: ', self.balance)
+
+    def add_income(self, income):
+        input('Enter income:\n')
+        cur.execute('UPDATE card SET balance = balance + ? WHERE number = ?', (self.card_number, income))
+        conn.commit()
+        self.balance += income
+        print('Income was added!')
+
+    def do_transfer(self, recipient, amount):
+        if not is_valid_card(recipient):
+            print('Probably you made a mistake in the card number. Please try again!')
+            return False
+        elif self.balance < amount:
+            print('')
+
+    def close_account(self):
+        pass
+
+
+def is_valid_card(card_number):
+    digits = []
+    for i, d in enumerate(card_number[:-1]):
+        if i % 2 == 0:
+            digits.append(2 * int(d) if 2 * int(d) <= 9 else 2 * int(d) - 9)
+        else:
+            digits.append(int(d) if int(d) <= 9 else int(d) - 9)
+    checksum = sum(digits, int(card_number[-1]))
+    return checksum % 10 == 0
+
+
+def new_card_number(number_of_account):
     card_number = "400000" + format(number_of_account + 1, "09")
     digits = []
     for i, d in enumerate(card_number):
@@ -39,7 +78,7 @@ def new_card(number_of_account):
 
 def create_account():
     number_of_acc = cur.execute("SELECT COUNT(*) FROM card;").fetchone()[0]
-    card_number, pin, balance = new_card(number_of_acc)
+    card_number, pin, balance = new_card_number(number_of_acc)
     cur.execute(
         "INSERT INTO card VALUES (?, ?, ?, ?);",
         (number_of_acc + 1, card_number, pin, balance),
@@ -64,17 +103,17 @@ def log_in():
 
 logged_in = False
 choice = None
-account_id = None
+current_account = None
 while choice != 0:
     choice = int(input(LOGGED_IN_PROMPT if logged_in else WELCOME_PROMPT))
     if not logged_in:
         if choice == 1:
             create_account()
         elif choice == 2:
-            logged_in, account_id = log_in()
+            logged_in, current_account = log_in()
     else:
         if choice == 1:
-            print("Balance: ", account_id[3])
+            print("Balance: ", current_account[3])
         else:
             logged_in = False
 
